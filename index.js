9463004c00cec46efeec10f3c81ad57a3e491235
matthew t muniz-silva <mmunizsi01@gmail.com>
2018-12-05 22:32:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // add solution here
+
+var newArray = [];
+
+function theBeatlesPlay(musicians, instruments){
+  for (var i = 0; i < musicians.length; i++){
+    musicians.push("John Lennon");
+    instruments.push("plays guitar");
+  }
+  console.log(musicians[0], instruments[0])
+}
+
+theBeatlesPlay(["sdlfalsk"],["afasdf"])
