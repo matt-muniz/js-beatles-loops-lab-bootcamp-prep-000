fa3c7035f6114eb6b7ef27a474b6cad55a597423
matthew t muniz-silva <mmunizsi01@gmail.com>
2018-12-05 22:00:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // add solution here
+
+var newArray = [];
+
+function theBeatlesPlay(musicians, instruments){
+  for (var i = 0; i <= musicians.length; i++){
+  }
+  console.log(musicians, instruments)
+}
+theBeatlesPlay(["hello"],["goodbye"])
