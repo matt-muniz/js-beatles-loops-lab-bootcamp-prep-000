578c78eb1cd38d5502ab174f50192bf5e7af5221
matthew t muniz-silva <mmunizsi01@gmail.com>
2018-12-06 03:03:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // add solution here
+
+var newArray = [];
+
+function theBeatlesPlay(musicians, instruments){
+  
+  for (var i = 0; i < musicians.length; i++){
+      newArray.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return newArray
+}
+
+
+function johnLennonFacts(facts){
+  var i = 0
+  while ( i < facts.length){
+    
+    facts.push(i)
+    console.log(facts)
+    i++
+    
+  }
+return facts
+}
+
+johnLennonFacts(["heloo"])
+johnLennonFacts(["hellloo"])
