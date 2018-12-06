13c4d37cbd1ee7eabbbfd0a3bfc907370585f953
matthew t muniz-silva <mmunizsi01@gmail.com>
2018-12-06 01:38:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // add solution here
+
+var newArray = [];
+
+function theBeatlesPlay(musicians, instruments){
+  
+  for (var i = 0; i <= newArray.length; i++){
+    newArray.push(`wlecome ${musicians[i]}`)
+    newArray.push(`welcome ${instruments[i]}`)
+    console.log(newArray)
+  }
+  
+  return newArray
+}
+
+theBeatlesPlay(["dafsd"],["afdsf"])
